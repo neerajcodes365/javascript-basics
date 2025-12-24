3aeb0dcef1207729ea93186ae06e7c582e7200ab
Neeraj J <neeraj2gokul4@gmail.com>
2025-12-24 13:36:03 +0530

    objects  completed

---
+++ reference_types.js
@@ -0,0 +1,13 @@
+
+let course={
+    name:"cse daa",
+    code:"cs4040e",
+    faculty:"vinod sir",
+    rating : 9
+};
+
+console.log(course);
+console.log(typeof(course));
+
+console.log(course.name);
+console.log(course['code']);
