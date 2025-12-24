fd21ea13c10c7c1840a294578eec6d28e256924c
Neeraj J <neeraj2gokul4@gmail.com>
2025-12-24 13:47:09 +0530

    arrays are also objects in js

---
+++ array.js
@@ -0,0 +1,11 @@
+let arr=['hi',2,null,true];
+
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+console.log(typeof(arr));
+
+console.log(arr);
+//arrays are also objects
