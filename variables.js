6f0ea5574b0cfde38b4440b3d060cb056b1aa6f2
Neeraj J <neeraj2gokul4@gmail.com>
2025-12-24 13:25:33 +0530

    variables completed

---
+++ variables.js
@@ -0,0 +1,24 @@
+
+let x="neeraj"
+console.log(x);
+console.log(typeof(x));
+
+let y=2;
+console.log(y);
+console.log(typeof(y));
+
+let z=2.3;
+console.log(z);
+console.log(typeof(z));
+
+let p=true;
+console.log(p);
+console.log(typeof(p));
+
+
+let a;
+console.log(a);
+console.log(typeof(a));
+a="neymar jr";
+console.log(a);
+console.log(typeof(a));
