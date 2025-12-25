90d7d0d9f692dbf1b232c5f887621749de75ab98
Neeraj J <neeraj2gokul4@gmail.com>
2025-12-25 12:22:36 +0530

    arrow functions

---
+++ arrow_functions.js
@@ -0,0 +1,12 @@
+let sum=function (a,b){
+    return a+b;
+}
+console.log(sum(5,4));
+
+let sumi=(a,b)=>{
+    return a+b;
+}
+//remove the function and put arrow after the parameters
+console.log(sumi(5,4));
+
+let sumii=(a,b)=>a+b;
